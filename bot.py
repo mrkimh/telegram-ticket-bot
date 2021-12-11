98f3eadc826e3dd0c35d11aabf1ff39f2e4e406c
Admin <spasov.mihail@ya.ru>
2021-12-11 07:47:41 +0300

    add admin features

--- bot.py
+++ bot.py
@@ -10,4 +10,35 @@ def process_start_command(message):
     bot.send_message(message.chat.id, "hello")
 
 
+@bot.message_handler(commands=['id'])
+def process_start_command(message: telebot.types.Message):
+    if message.chat.type == "private":
+        bot.send_message(message.chat.id, message.chat.id)
+
+
+@bot.message_handler(commands=[f'{add_first_admin_command()}'])
+def process_afa_command(message: telebot.types.Message):
+    try:
+        add_new_admin(message.chat.id)
+        bot.reply_to(message, "Вы успешно получили админ права")
+    except Exception as e:
+        bot.reply_to(message, "error")
+        print(e)
+
+
+@bot.message_handler(commands=['addadmin'])
+def process_add_admin_command(message: telebot.types.Message):
+    if validate_admin(message.from_user.id):
+        words = message.text.split()
+        if len(words) == 2 and words[1].isnumeric():
+            try:
+                add_new_admin(int(words[1]))
+                bot.send_message(message.chat.id, "Админ успешно добавлен")
+            except Exception as e:
+                bot.reply_to(message, "error")
+                print(e)
+    else:
+        bot.send_message(message.chat.id, "Недостаточно прав")
+
+
 bot.infinity_polling()
--- utils.py
+++ utils.py
@@ -1,4 +1,42 @@
+import sqlite3
+
+
 def load_token():
     with open("./local_storage/token.txt", "r") as file:
         token = file.read()
-        return token
+    return token
+
+
+def add_first_admin_command():
+    with open("./local_storage/add_first_admin_command.txt", "r") as file:
+        command = file.read()
+    return command
+
+
+def add_new_admin(tg_id: int):
+    connection = establish_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(f"insert into admin_users (telegram_user_id) values ({tg_id});")
+    connection.commit()
+    return validate_admin(tg_id)
+
+
+def validate_admin(tg_id: int):
+    connection = establish_db_connection()
+    cursor = establish_db_connection().cursor()
+    cursor.execute(f"select * from ADMIN_USERS where telegram_user_id = {tg_id}")
+    result = cursor.fetchall()
+    return len(result) > 0
+
+
+def establish_db_connection():
+    connection = sqlite3.connect("./local_storage/db/telegram_bot.db")
+    return connection
+
+
+# con = establish_db_connection()
+# cur = con.cursor()
+# cur.execute(f"insert into admin_users (telegram_user_id) values (55555555);")
+# con.commit()
+
+

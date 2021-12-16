d8c1a12fd7da8d6870011259473d73ff40b5faf9
Admin <spasov.mihail@ya.ru>
2021-12-16 16:17:33 +0300

    add personal info

--- bot.py
+++ bot.py
@@ -3,11 +3,19 @@ from utils import *
 import logging
 
 bot = telebot.TeleBot(load_token())
+context = dict()
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help', 'info'])
 def process_start_command(message):
+    if not user_exists(message.chat.id):
+        add_or_update_user(message.chat.id)
     bot.send_message(message.chat.id, "hello")
+    info_markup = telebot.types.InlineKeyboardMarkup()
+    info_markup.add(telebot.types.InlineKeyboardButton(text="Заполнить анкету", callback_data="user_info accept"))
+    info_markup.add(telebot.types.InlineKeyboardButton(text="Отказаться", callback_data="user_info decline"))
+    bot.send_message(message.chat.id, "Для более эффективного взаимодействия с ботом рекомендуется заполнить основную "
+                                      "информацию о себе", reply_markup=info_markup)
 
 
 @bot.message_handler(commands=['id'])
@@ -39,6 +47,98 @@ def process_add_admin_command(message: telebot.types.Message):
                 print(e)
     else:
         bot.send_message(message.chat.id, "Недостаточно прав")
+
+
+@bot.message_handler(commands=['faq'])
+def process_faq_command(message: telebot.types.Message):
+    categories = get_categories()
+    categories_markup = telebot.types.InlineKeyboardMarkup()
+    for category in categories:
+        categories_markup.add(telebot.types.InlineKeyboardButton(text=f"{category[0]}. {category[1]}",
+                                                                 callback_data=f"faq {category[0]}"))
+    bot.send_message(message.chat.id, "Разделы:", reply_markup=categories_markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call: telebot.types.CallbackQuery):
+    if call.message and call.data:
+        if call.data.split()[0] == "faq":
+            questions = get_questions(int(call.data.split()[1]))
+            reply = ""
+            for q in questions:
+                reply += f"{q[0]}. {q[1]}\n {q[2]}\n\n"
+            bot.send_message(call.message.chat.id, reply)
+        if call.data.split()[0] == "user_info":
+            if call.data.split()[1] == "decline":
+                bot.send_message(call.message.chat.id, "Вы в любое время можете заполнить или изменить анкету. "
+                                                       "Для этого используйте команду /info")
+            elif call.data.split()[1] == "accept":
+                context[call.from_user.id] = UserStatus.name
+                fill_user_info(call.from_user.id)
+        if call.data.split()[0] == "skip":
+            if call.data.split()[1] == "name" and context.get(call.message.chat.id) == UserStatus.name:
+                context.pop(call.message.chat.id)
+                context[call.message.chat.id] = UserStatus.email
+                fill_user_info(call.message.chat.id)
+            elif call.data.split()[1] == "email" and context.get(call.message.chat.id) == UserStatus.email:
+                context.pop(call.message.chat.id)
+                context[call.message.chat.id] = UserStatus.grade
+                fill_user_info(call.message.chat.id)
+            elif call.data.split()[1] == "grade" and context.get(call.message.chat.id) == UserStatus.grade:
+                context.pop(call.message.chat.id)
+                context[call.message.chat.id] = UserStatus.fill_compl
+                fill_user_info(call.message.chat.id)
+
+
+def fill_user_info(user):
+    status = context[user]
+    if status == UserStatus.name:
+        name_markup = telebot.types.InlineKeyboardMarkup()
+        name_markup.add(telebot.types.InlineKeyboardButton(text="Пропустить", callback_data="skip name"))
+        bot.send_message(user, "Ваше имя?", reply_markup=name_markup)
+    elif status == UserStatus.email:
+        email_markup = telebot.types.InlineKeyboardMarkup()
+        email_markup.add(telebot.types.InlineKeyboardButton(text="Пропустить", callback_data="skip email"))
+        bot.send_message(user, "Ваш Email?", reply_markup=email_markup)
+    elif status == UserStatus.grade:
+        grade_markup = telebot.types.InlineKeyboardMarkup()
+        grade_markup.add(telebot.types.InlineKeyboardButton(text="Пропустить", callback_data="skip grade"))
+        bot.send_message(user, "Ваш класс?", reply_markup=grade_markup)
+    elif status == UserStatus.fill_compl:
+        context.pop(user)
+        bot.send_message(user, "Спасибо! Информация сохранена. Вы можете обновить ее в любое время при помощи команды "
+                               "/info")
+
+
+@bot.message_handler(func=lambda message: True)
+def process_message(message: telebot.types.Message):
+    if not context.get(message.chat.id) is None:
+        user, name, email, grade = get_user(message.chat.id)
+        if context.get(message.chat.id) == UserStatus.name:
+            name = message.text
+            add_or_update_user(user, name, email, grade)
+            context.pop(user)
+            context[user] = UserStatus.email
+            fill_user_info(user)
+        elif context.get(message.chat.id) == UserStatus.email:
+            email = message.text
+            add_or_update_user(user, name, email, grade)
+            context.pop(user)
+            context[user] = UserStatus.grade
+            fill_user_info(user)
+        elif context.get(message.chat.id) == UserStatus.grade:
+            grade = message.text
+            add_or_update_user(user, name, email, grade)
+            context.pop(user)
+            context[user] = UserStatus.fill_compl
+            fill_user_info(user)
+
+    if message.chat.type == "private" and message.text.find("?") > 0 and not validate_admin(message.chat.id):
+        process_new_ticket(message)
+
+
+def process_new_ticket(message: telebot.types.Message):
+    pass
 
 
 bot.infinity_polling()
--- utils.py
+++ utils.py
@@ -1,10 +1,16 @@
 import sqlite3
+from enum import Enum
 
 
 def load_token():
     with open("./local_storage/token.txt", "r") as file:
         token = file.read()
     return token
+
+
+def establish_db_connection():
+    connection = sqlite3.connect("./local_storage/db/telegram_bot.db")
+    return connection
 
 
 def add_first_admin_command():
@@ -23,20 +29,67 @@ def add_new_admin(tg_id: int):
 
 def validate_admin(tg_id: int):
     connection = establish_db_connection()
-    cursor = establish_db_connection().cursor()
+    cursor = connection.cursor()
     cursor.execute(f"select * from ADMIN_USERS where telegram_user_id = {tg_id}")
     result = cursor.fetchall()
     return len(result) > 0
 
 
-def establish_db_connection():
-    connection = sqlite3.connect("./local_storage/db/telegram_bot.db")
-    return connection
+def get_categories():
+    connection = establish_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("select * from categories")
+    return cursor.fetchall()
 
 
-# con = establish_db_connection()
-# cur = con.cursor()
-# cur.execute(f"insert into admin_users (telegram_user_id) values (55555555);")
-# con.commit()
+def get_questions(category_id: int):
+    connection = establish_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(f"select id,question,answer from questions where category={category_id}")
+    return cursor.fetchall()
+
+
+def user_exists(tg_id: int):
+    connection = establish_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(f"select * from users where telegram_user_id = {tg_id}")
+    result = cursor.fetchall()
+    return len(result) > 0
+
+
+def add_or_update_user(tg_id, name=None, email=None, grade=None):
+    if name is None:
+        name = "NULL"
+    if email is None:
+        email = "NULL"
+    if grade is None:
+        grade = "NULL"
+    connection = establish_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(f"INSERT OR REPLACE into users (telegram_user_id, name, email, grade) values ({tg_id}, \"{name}\", \"{email}\", \"{grade}\")")
+    connection.commit()
+
+
+def get_user(tg_id):
+    connection = establish_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(f"select * from users where telegram_user_id = {tg_id}")
+    res = cursor.fetchone()
+    if len(res) > 0:
+        return res
+    else:
+        return None
+
+
+def add_new_ticket(user_id):
+    pass
+
+
+class UserStatus(Enum):
+    name = 1
+    email = 2
+    grade = 3
+    fill_compl = 4
+
 
 
